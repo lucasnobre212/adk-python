34ee8f5254ccc53ea463cd37e89f2d8a6b8bc96c
Lucas Nobre <lucaas.sn@gmail.com>
2025-05-17 10:12:55 -0300

    Removed annoying warning

--- src/google/adk/tools/function_parameter_parse_util.py
+++ src/google/adk/tools/function_parameter_parse_util.py
@@ -56,10 +56,10 @@ def _update_for_default_if_mldev(schema: types.Schema):
   if schema.default is not None:
     # TODO(kech): Remove this workaround once mldev supports default value.
     schema.default = None
-    logger.warning(
-        'Default value is not supported in function declaration schema for'
-        ' Google AI.'
-    )
+    # logger.warning(
+    #     'Default value is not supported in function declaration schema for'
+    #     ' Google AI.'
+    # )
 
 
 def _raise_if_schema_unsupported(variant: str, schema: types.Schema):
